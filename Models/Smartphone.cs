ca6229325557f17059268dae5d6e90119b81c73f
felipevinicios <felipe.1577@outlook.com>
2024-01-02 11:30:25 -0300

    git commit -m

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,13 +2,29 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
+        
+
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get ; set ; }
+        private string Imei {get; set;}
 
-        public Smartphone(string numero)
+        private int Memoria { get; set;}
+
+
+
+        public  Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+
+
             // TODO: Passar os parâmetros do construtor para as propriedades
+            
+
+
         }
 
         public void Ligar()
@@ -21,6 +37,9 @@ namespace DesafioPOO.Models
             Console.WriteLine("Recebendo ligação...");
         }
 
-        public abstract void InstalarAplicativo(string nomeApp);
+        public virtual void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine(nomeApp);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartohone Nokia: ");
+Smartphone nokia = new Nokia (numero: "11970335014", modelo: "735", imei: "155515",memoria :16);
+nokia.Ligar();
+nokia.InstalarAplicativo("facebook");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iphone: ");
+Smartphone iphone = new Iphone (numero: "11945151856", modelo: "6s", imei:"698545",memoria:32);
+iphone.Ligar();
+iphone.InstalarAplicativo("whastsapp");
